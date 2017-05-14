f490a0fe4bdfce1b94860c85d274c31770618df0
IvelliosR <radek19941@gmail.com>
2017-05-14 02:51:02 +0200

    debug network

--- www/js/products-data-functions.js
+++ www/js/products-data-functions.js
@@ -18,10 +18,24 @@ function getConnectionInterval() {
 function checkConnection() {
 	
 	var networkState = navigator.connection.type;
-	if (networkState == Connection.NONE) {
+ 
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+	
+	alert('Connection type: ' + states[networkState]);
+	
+	//var networkState = navigator.connection.type;
+	//if (networkState == Connection.NONE) {
 		return false;
-	}
-	return true;
+	//}
+	//return true;
 	
 }
 
