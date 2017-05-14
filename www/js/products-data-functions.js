17f670eb21198598f30c738c855f0dc9b9664dcf
IvelliosR <radek19941@gmail.com>
2017-05-14 02:55:00 +0200

    get connect check to try

--- www/js/products-data-functions.js
+++ www/js/products-data-functions.js
@@ -17,25 +17,17 @@ function getConnectionInterval() {
 
 function checkConnection() {
 	
-	var networkState = navigator.connection.type;
- 
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
-	
-	alert('Connection type: ' + states[networkState]);
-	
-	//var networkState = navigator.connection.type;
-	//if (networkState == Connection.NONE) {
+	try {
+		var networkState = navigator.connection.type;
+		if (networkState == Connection.NONE) {
+			return false;
+		}
+		return true;
+	}
+	catch(err) {
 		return false;
-	//}
-	//return true;
+	}
+	
 	
 }
 
