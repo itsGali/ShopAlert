7f4a9f24cab46e9c07b937528a8947c4fb3589a9
IvelliosR <radek19941@gmail.com>
2017-05-17 21:47:38 +0200

    add find number

--- www/js/received-lists-draw-functions.js
+++ www/js/received-lists-draw-functions.js
@@ -60,7 +60,7 @@ function receivedProductsListDrawNewList(list) {
 	var link = $("<a/>", {
 		href: "#receivedList"
 	}).click(function() {
-		setReceivedListSourceByNumber(list.sourceNumber, $("#receivedListSource .value"))
+		findSingleListSourceContact(list.sourceNumber, $("#receivedListSource .value"))
 		receivedListsOpenList(list.id);
 		receivedListDraw(list.id);
 	}).append(container);
@@ -101,7 +101,7 @@ function receivedProductsListDrawOpenList(list) {
 	var link = $("<a/>", {
 		href: "#receivedList"
 	}).click(function() {
-		setReceivedListSourceByNumber(list.sourceNumber, $("#receivedListSource .value"))
+		findSingleListSourceContact(list.sourceNumber, $("#receivedListSource .value"))
 		receivedListDraw(list.id);
 	})
 	.append(container);
@@ -142,7 +142,7 @@ function receivedProductsListDrawCloseList(list) {
 	var link = $("<a/>", {
 		href: "#receivedList"
 	}).click(function() {
-		setReceivedListSourceByNumber(list.sourceNumber, $("#receivedListSource .value"))
+		findSingleListSourceContact(list.sourceNumber, $("#receivedListSource .value"))
 		receivedListDraw(list.id);
 	})
 	.append(container);
